b237d70bd4381fd9eb2bc10b5c73e9c355f53fa6
Alex Aguirrebena <alex.aguirrebena@gmail.com>
2020-08-20 12:37:30 -0700

    File structure adjust

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
+// import './App.css';
 import Navigation from "./components/Navigation";
 import NewPlayer from "./pages/NewPlayer"
 import Landing from "./pages/Landing"
@@ -8,27 +8,27 @@ import NewTournament from "./pages/NewTournament"
 import Start from "./components/Start"
 import {AuthProvider} from "./Auth";
 import PrivateRoute from "./PrivateRoute";
+import "./App.scss";
+import Splash from "./pages/Splash";
 
 function App() {
   return (
     <AuthProvider>
       <div className="App">
         <Router>
-        <Navigation />
+        {/* <Navigation /> */}
         <Switch>
-            <PrivateRoute exact path="/" component={Landing}/>
-            <Route exact path="/newplayer" component={NewPlayer}/>
-            <Route exact path="/newtournament" component={NewTournament}/>
+            <Route exact path="/login" component={Splash}/>
+            {/* <Route exact path="/newplayer" component={NewPlayer}/>
+            <Route exact path="/newtournament" component={NewTournament}/> */}
           </Switch>
-        {/* <Start></Start> */}
-        {/* <NewPlayer /> */}
-        {/* <Landing /> */}
-        {/* <NewTournament /> */}
-        </Router>
+     
+       
+         </Router>
         
-      </div>
+       </div>
     </AuthProvider>
-  );
-}
+   );
+ }
 
 export default App;
