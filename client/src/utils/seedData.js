0f93eb7755518e195bb08f6dfc6b6101474f57ba
Jen Jayme <jeniferjayme@gmail.com>
2020-08-29 13:05:09 -0700

    Added MyFriends panel to MyHome, and added seedData.js

---
+++ client/src/utils/seedData.js
@@ -0,0 +1,22 @@
+CREATE DATABASE statdashdb;
+
+USE statdashdb;
+
+INSERT INTO Performances (id, gulagKills, gulagDeaths, kills, deaths, damage, placement, revives, clutchKills, damageToKills, overallScore, startMatch, createdAt, updatedAt, TournamentId, UserId)
+VALUES ('10',21,12,64,3,51,3,22,19,55,103,null,'2020-08-28 04:56:40', '2020-08-28 04:56:40','1','1');
+INSERT INTO Performances (id, gulagKills, gulagDeaths, kills, deaths, damage, placement, revives, clutchKills, damageToKills, overallScore, startMatch, createdAt, updatedAt, TournamentId, UserId)
+VALUES ('20',12,33,52,2,59,1,33,12,43,99,null,'2020-08-28 04:56:40', '2020-08-28 04:56:40','1','2');
+INSERT INTO Performances (id, gulagKills, gulagDeaths, kills, deaths, damage, placement, revives, clutchKills, damageToKills, overallScore, startMatch, createdAt, updatedAt, TournamentId, UserId)
+VALUES ('30',17,23,57,2,65,2,29,16,52,92,null,'2020-08-28 04:56:40', '2020-08-28 04:56:40','1','3');
+INSERT INTO Performances (id, gulagKills, gulagDeaths, kills, deaths, damage, placement, revives, clutchKills, damageToKills, overallScore, startMatch, createdAt, updatedAt, TournamentId, UserId)
+
+
+VALUES ('40',25,28,49,4,61,1,35,20,61,107,null,'2020-08-28 04:56:40', '2020-08-28 04:56:40','1','4');
+INSERT INTO Users (id, firstName, lastName, email, gamerTag, platform, activisionId, username, password, createdAt, updatedAt)
+VALUES ('1', 'Jen', 'Smith', 'j@j.com', 'yungsnowflake510', 'Playstation - (psn)', '', 'jen', '', '2020-08-28 04:56:40', '2020-08-28 04:56:40');
+INSERT INTO Users (id, firstName, lastName, email, gamerTag, platform, activisionId, username, password, createdAt, updatedAt)
+VALUES ('2','Stacey', 'Yoon', 's@s.com', 'sumcap', 'Playstation - (psn)', '', 'Stacey', '', '2020-08-28 04:56:40', '2020-08-28 04:56:40');
+INSERT INTO Users (id, firstName, lastName, email, gamerTag, platform, activisionId, username, password, createdAt, updatedAt)
+VALUES ('3', 'Luke', 'Skywalker', 'luke@gmail.com', 'useTheForceLuke', 'Playstation - (psn)', '', 'luke', '', '2020-08-28 04:56:40', '2020-08-28 04:56:40');
+INSERT INTO Users (id, firstName, lastName, email, gamerTag, platform, activisionId, username, password, createdAt, updatedAt)
+VALUES ('4', 'Geoff', 'Yo', 'geoff@gmail.com', 'roycedub510', 'Playstation - (psn)', '', 'royce', '', '2020-08-28 04:56:40', '2020-08-28 04:56:40');
