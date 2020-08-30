f55192aa1d9c135e47ed616920859e6a63f8dad7
Jen Jayme <jeniferjayme@gmail.com>
2020-08-29 21:20:16 -0700

    Added partial deployment code for sequelize to heroku in server.js & models.

--- server.js
+++ server.js
@@ -9,23 +9,35 @@ const routes = require("./routes/cod-route"); // api routes for db
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-  console.log("App connected to JAWS and listening on PORT " + "http://localhost:" + PORT);
-} else {
-  connection = mysql.createConnection({
-    host: 'dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'sbbg3zakw7rqxuc7',
-    password: 'upgtoueg3fmejp2b',
-    database: 'zlr8wau3cuiweniv'
-  });
-};
-// Define API routes here
+//=======================================================================
+// This section was per instructions for using MongoDB
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+//   console.log("App connected to JAWS and listening on PORT " + "http://localhost:" + PORT);
+// } else {
+//   connection = mysql.createConnection({
+//     host: 'dno6xji1n8fm828n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//     user: 'sbbg3zakw7rqxuc7',
+//     password: 'upgtoueg3fmejp2b',
+//     database: 'zlr8wau3cuiweniv'
+//   });
+// };
+//=======================================================================
+
+//This section comes from instructions for deploying with Sequelize to Heroku
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// development only
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
 
 // Send every other request to the React app
 // Define any API routes before this runs
